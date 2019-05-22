863e9086043d9ac2c8a0546e0a85f35cdce72490
Ethan Sagin <ethansagin@gmail.com>
2019-05-22 21:56:12 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,7 +2,7 @@ class Pokemon
   attr_accessor :name, :type
   attr_reader :id
   
-  def initialize(id: nil, name:, type:, db:)
+  def initialize(id: nil, name:, type:, db: db/pokemon.db)
     @name = name
     @type = type
   end
